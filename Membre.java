a629a46634e5b1b7bb6cac9da09dca91cab69b6e
ufzrihfuizr <144798872+ufzrihfuizr@users.noreply.github.com>
2025-10-18 17:50:42 +0200

    Add files via upload

---
+++ Membre.java
@@ -0,0 +1,34 @@
+public class Membre {
+    private String nom;
+    private int id;
+    private List<Media> mediasEmpruntes;
+
+    public Membre(String nom, int id) {
+        this.nom = nom;
+        this.id = id;
+        this.mediasEmpruntes = new ArrayList<>();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void emprunterMedia(Media media) {
+        if (media != null) {
+            mediasEmpruntes.add(media);
+        }
+    }
+
+    public List<Media> getMediasEmpruntes() {
+        return mediasEmpruntes;
+    }
+
+    @Override
+    public String toString() {
+        return "Membre{" + "nom='" + nom + "', id=" + id + "}";
+    }
+}
