211e89952e2b42df2cefee8b62271ad957af1803
ufzrihfuizr <144798872+ufzrihfuizr@users.noreply.github.com>
2025-10-18 18:07:44 +0200

    Update livre.java

--- livre.java
+++ livre.java
@@ -1,23 +1,26 @@
+//heritage de l'ensemble des variables,methodes,propriétés... de la classe Media depuis la classe Livre
 public class Livre extends Media {
+    //initialisation des variables utilisées dans la classe
     private String auteur;
     private int nbPages;
-
+//constructeur de la classe paramétré
     public Livre(String titre, int anneePublication, String auteur, int nbPages) {
         super(titre, anneePublication);
         this.auteur = auteur;
         this.nbPages = nbPages;
     }
-
+//getter pour acceder au nom de l'auteur
     public String getAuteur() {
         return auteur;
     }
-
+//getter pour acceder au nombre de pages
     public int getNbPages() {
         return nbPages;
     }
-
+//ecrasement de la valeur retournée depuis la classe mère de la methode getDescription
     @Override
     public String getDescription() {
         return "Livre de " + auteur + ", " + nbPages + " pages.";
     }
+
 }
