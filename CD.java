e609f19b0e640894d18201f2802abdd4fea7c4a9
ufzrihfuizr <144798872+ufzrihfuizr@users.noreply.github.com>
2025-10-18 17:59:09 +0200

    Update CD.java

--- CD.java
+++ CD.java
@@ -1,23 +1,27 @@
 public class CD extends Media {
+    //initialisationdes variables utilisées
     private String artiste;
     private int duree;  // en minutes
 
+    //constructeur paramétré
     public CD(String titre, int anneePublication, String artiste, int duree) {
         super(titre, anneePublication);
         this.artiste = artiste;
         this.duree = duree;
     }
-
+    //getter pour les artistes
     public String getArtiste() {
         return artiste;
     }
-
+    //getter pour la durée
     public int getDuree() {
         return duree;
     }
 
+    //modification(écrasement) de la methode getDescription initialement declaré dans la classe Media et hérité depuis la class CD
     @Override
     public String getDescription() {
         return "CD de " + artiste + ", durée : " + duree + " min.";
     }
+
 }
