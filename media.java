1449a2c2e429562ed057a2aca8faee8287d48c22
ufzrihfuizr <144798872+ufzrihfuizr@users.noreply.github.com>
2025-10-18 18:04:07 +0200

    Update media.java

--- media.java
+++ media.java
@@ -1,29 +1,31 @@
 public abstract class Media {
+    //initialisation des variables utilisées dans la classe
     private String titre;
     private int anneePublication;
-
+//constructeur non paramétré
     public Media() {
         this.titre = "";
         this.anneePublication = 0;
     }
-
+//constructeur paramétré
     public Media(String titre, int anneePublication) {
         this.titre = titre;
         this.anneePublication = anneePublication;
     }
-
+//getter pour les titres
     public String getTitre() {
         return titre;
     }
-
+//getteur pour les années de parutions
     public int getAnneePublication() {
         return anneePublication;
     }
-
+//declaration de la methode getDescription accessible uniquement depuis ces sous classes
     public abstract String getDescription();
-
+//declaration de la methode to string retournant en sortie une chaine de caractère indiquant le titre et l'année de parution
     @Override
     public String toString() {
         return titre + " (" + anneePublication + ")";
     }
+
 }
