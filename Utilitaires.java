fd616055b324a2ca1e69b601af8f18bb671b0b86
ufzrihfuizr <144798872+ufzrihfuizr@users.noreply.github.com>
2025-10-19 00:18:58 +0200

    Update Utilitaires.java

--- Utilitaires.java
+++ Utilitaires.java
@@ -5,27 +5,30 @@ import java.util.stream.Collectors;
 
 public class Utilitaires {
 
-    // Étape 4.1 : afficherListe
+    //initialisation d'une methode static générique retournant l'ensemble des éléments présent au sein d'une liste de classe quelquonque <T>
     public static <T> void afficherListe(List<T> liste) {
         if (liste == null) return;
         for (T elt : liste) {
+            //appelle implicitement la methode toString
             System.out.println(elt);
         }
     }
 
-    // Étape 4.2 : filtrer
+    //filtrage
     public static <T> List<T> filtrer(List<T> liste, Predicate<T> critere) {
         if (liste == null) return null;
+        //convertit la collection en un flux de données compatibles aux methodes filter , collect
         return liste.stream()
-                    .filter(critere)
-                    .collect(Collectors.toList());
+                    .filter(critere) //filtrage par predicat
+                    .collect(Collectors.toList()); //conversion inverse traduisant le flux convertit dans une nouvelle collection à l'aide de la methode toList()
     }
 
-    // Étape 6.1 : copierCollection
+    // Copie
     public static <T> void copierCollection(Collection<T> source, Collection<T> destination) {
         if (source == null || destination == null) return;
         for (T elt : source) {
             destination.add(elt);
         }
     }
+
 }
